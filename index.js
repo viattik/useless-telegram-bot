3f09a5f01adb8cacbf20c61ce5691e9a28e851ce
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-02-10 17:12:13 +0200

    Prod

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 const express = require('express');
 const app = express();
-// const TelegramBot = require('node-telegram-bot-api'),
-// // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-// 		telegram = new TelegramBot("349286184:AAFJQPLU0xbQAvd8HcB6I2nMp5xrpwzV7KU", { polling: true });
+const TelegramBot = require('node-telegram-bot-api'),
+// Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+		telegram = new TelegramBot("349286184:AAFJQPLU0xbQAvd8HcB6I2nMp5xrpwzV7KU", { polling: true });
 
 app.set('port', (process.env.PORT || 5000));
 
 
 app.get('/', function (req, res) {
-	res.json({ 1: 2});
+	res.send('Nothing is here');
 	res.end();
 });
 
@@ -38,14 +38,13 @@ function getReply(text) {
 // 	]);
 // });
 
-// telegram.on("text", (message) => {
-// 	console.log(message);
-// 	const text = message.text.toLowerCase();
-// 	const reply = getReply(text);
-// 	telegram.sendMessage(message.chat.id, reply, {
-// 		parse_mode: "Markdown"
-// 	});
-// });
+telegram.on("text", (message) => {
+	const text = message.text.toLowerCase();
+	const reply = getReply(text);
+	telegram.sendMessage(message.chat.id, reply, {
+		parse_mode: "Markdown"
+	});
+});
 
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
