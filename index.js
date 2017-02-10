e6f0a04a6f8c9841976a481d9852bb62dd255968
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-02-10 16:07:06 +0200

    Remove telegram bot

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 const express = require('express');
 const app = express();
-const TelegramBot = require('node-telegram-bot-api'),
-// Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-		telegram = new TelegramBot("349286184:AAFJQPLU0xbQAvd8HcB6I2nMp5xrpwzV7KU", { polling: true });
+// const TelegramBot = require('node-telegram-bot-api'),
+// // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+// 		telegram = new TelegramBot("349286184:AAFJQPLU0xbQAvd8HcB6I2nMp5xrpwzV7KU", { polling: true });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -23,20 +23,20 @@ function getReply(text) {
 	}
 }
 
-telegram.on("inline_query", (query) => {
-	var searchTerm = query.query.trim();
-	console.log(searchTerm);
-	telegram.answerInlineQuery(query.id, [
-		{
-			type: "article",
-			id: "testarticle",
-			title: "Hello world",
-			input_message_content: {
-				message_text: "Hello, world! This was sent from my super cool inline bot."
-			}
-		}
-	]);
-});
+// telegram.on("inline_query", (query) => {
+// 	var searchTerm = query.query.trim();
+// 	console.log(searchTerm);
+// 	telegram.answerInlineQuery(query.id, [
+// 		{
+// 			type: "article",
+// 			id: "testarticle",
+// 			title: "Hello world",
+// 			input_message_content: {
+// 				message_text: "Hello, world! This was sent from my super cool inline bot."
+// 			}
+// 		}
+// 	]);
+// });
 
 // telegram.on("text", (message) => {
 // 	console.log(message);
