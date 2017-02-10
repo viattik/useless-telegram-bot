2978697e3138d3ec18ad65a1c1b93a8d331f6a29
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-02-10 16:03:01 +0200

    Init telegram bot

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const TelegramBot = require('node-telegram-bot-api'),
+// Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+		telegram = new TelegramBot("349286184:AAFJQPLU0xbQAvd8HcB6I2nMp5xrpwzV7KU", { polling: true });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,6 +12,40 @@ app.get('/', function (req, res) {
 	res.json({ 1: 2});
 	res.end();
 });
+
+function getReply(text) {
+	if (text.includes('ничо')) {
+		return 'Вот и хорошо';
+	} else if (text.includes('/start')) {
+		return 'Давай, расскажи мне о своих проблемах.';
+	} else {
+		return 'И чо?';
+	}
+}
+
+telegram.on("inline_query", (query) => {
+	var searchTerm = query.query.trim();
+	console.log(searchTerm);
+	telegram.answerInlineQuery(query.id, [
+		{
+			type: "article",
+			id: "testarticle",
+			title: "Hello world",
+			input_message_content: {
+				message_text: "Hello, world! This was sent from my super cool inline bot."
+			}
+		}
+	]);
+});
+
+// telegram.on("text", (message) => {
+// 	console.log(message);
+// 	const text = message.text.toLowerCase();
+// 	const reply = getReply(text);
+// 	telegram.sendMessage(message.chat.id, reply, {
+// 		parse_mode: "Markdown"
+// 	});
+// });
 
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
