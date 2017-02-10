1c7bd55c33000655008c43ef426b37f5df1e02af
Andrei Sierkov <andrei.sierkov@railsreactor.com>
2017-02-10 15:23:44 +0200

    heroku test

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function (req, res) {
+	res.json({ 1: 2});
+	res.end();
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
